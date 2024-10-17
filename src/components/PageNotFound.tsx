662c240b03ca3e699450b6cd36b1a994e166d4d3
anuj-kumary <anujf0510@gmail.com>
2024-10-17 10:32:24 +0530

    Fix page not found minor stylling issue

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,6 +1,6 @@
 export const PageNotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex w-full flex-col items-center m-20 h-screen b">
     <div className="relative">
       <svg
         className="animate-bounce w-40 h-40 text-gray-400"
@@ -16,7 +16,7 @@ export const PageNotFound = () => {
           d="M12 8v4m0 4h.01M19 12c0 3.866-3.134 7-7 7s-7-3.134-7-7 3.134-7 7-7 7 3.134 7 7z"
         />
       </svg>
-      <div className="absolute top-10 left-0 right-0">
+      <div className="absolute left-0 right-0">
         <h1 className="text-6xl text-gray-600 font-bold mb-4 text-center">404</h1>
         <p className="text-2xl text-gray-500 text-center">Page Not Found</p>
         <p className="text-gray-400 text-center">
