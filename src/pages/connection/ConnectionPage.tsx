f1cdeca5bd58c6b78d4b6880a45f94ff0e340247
anuj-kumary <anujf0510@gmail.com>
2024-11-04 11:49:48 +0530

    Fix card layout

--- src/pages/connection/ConnectionPage.tsx
+++ src/pages/connection/ConnectionPage.tsx
@@ -9,7 +9,7 @@ const cardData = [
 
 function ConnectionPage() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+    <div className="grid flex-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
     {cardData.map((card) => (
       <Card key={card.id} name={card.name} role={card.role} image={card.image} />
     ))}
