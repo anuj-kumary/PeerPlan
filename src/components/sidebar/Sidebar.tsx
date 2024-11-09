05e90e6f2fc7dbb71ad0921a2d43c6cd114aa87f
anuj-kumary <anujf0510@gmail.com>
2024-11-09 10:27:15 +0530

    nit

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useIsLoggedIn from "../../hooks/useIsLoggedIn";
 
 export const Sidebar = () => {
     const [activeTab, setActiveTab] = useState('home');
+    const { isLoggedIn } = useIsLoggedIn();
     return (
         <aside className="bg-white shadow-lg hidden md:flex md:flex-col w-64  text-black flex-col">
             <div className="flex-grow">
                 <ul className="mt-4">
                     <li className={`px-4 mb-2 py-2 transition duration-200 ease-in-out  cursor-pointer ${activeTab === 'home' ? 'bg-[#3b58ff] text-white' : 'hover:bg-gray-200'}`}
                         onClick={() => setActiveTab('home')}>
-                        <Link  to="/home" className="block">Home</Link>
+                        <Link to="/home" className="block">Home</Link>
                     </li>
                     <li className={`px-4 mb-2 py-2 transition duration-200 ease-in-out  cursor-pointer ${activeTab === 'connection' ? 'bg-[#3b58ff] text-white' : 'hover:bg-gray-200'}`}
                         onClick={() => setActiveTab('connection')}>
@@ -19,13 +21,16 @@ export const Sidebar = () => {
             </div>
             <div className="flex flex-col mt-auto">
                 <ul>
-                    <li className={`px-4 mb-2 py-2 transition duration-200 ease-in-out  cursor-pointer ${activeTab === 'profile' ? 'bg-[#3b58ff] text-white' : 'hover:bg-gray-200'}`}
-                        onClick={() => setActiveTab('profile')}>
-                        <Link to="/profile" className="block">Profile</Link>
-                    </li>
-                    <li className="px-4 transition duration-200 ease-in-out  py-2 hover:bg-gray-700">
-                        <a href="#" className="block">Logout</a>
-                    </li>
+                    {
+                        isLoggedIn &&
+                        <><li className={`px-4 mb-2 py-2 transition duration-200 ease-in-out  cursor-pointer ${activeTab === 'profile' ? 'bg-[#3b58ff] text-white' : 'hover:bg-gray-200'}`}
+                            onClick={() => setActiveTab('profile')}>
+                            <Link to="/profile" className="block">Profile</Link>
+                        </li><li className="px-4 transition duration-200 ease-in-out  py-2 hover:bg-gray-700">
+                                <a href="#" className="block">Logout</a>
+                            </li></>
+                    }
+
                 </ul>
             </div>
         </aside>
