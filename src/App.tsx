54ce14a640903b6df6f614330d815de98a61a659
anuj-kumary <anujf0510@gmail.com>
2024-11-15 14:55:50 +0530

    nit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from "react-hot-toast"
 import AppRoutes from "./AppRoutes"
 
 export function App() {
   return (
     <>
       <div>
+        <Toaster position="top-center" reverseOrder={false}
+        />
         <AppRoutes />
       </div>
 
